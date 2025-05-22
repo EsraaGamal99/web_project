0e80a2c29189f3cf6d6cf0c36f6d000c8c1f259e
esraaEcmpp <esraa.gamal@ecmpp.com>
2025-05-23 01:17:01 +0300

    create home_web_screen

--- lib/features/home/ui/screens/home_web_screen.dart
+++ lib/features/home/ui/screens/home_web_screen.dart
@@ -1,45 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:web_project/core/theming/colors.dart';
+import 'package:web_project/core/theming/text_styles.dart';
+import 'package:web_project/core/widgets/custom_appbar.dart';
+import 'package:web_project/features/home/ui/screens/items_screen.dart';
 
 class HomeWebScreen extends StatefulWidget {
-  const HomeWebScreen({super.key,});
+   HomeWebScreen({super.key});
 
   @override
   State<HomeWebScreen> createState() => _HomeWebScreenState();
 }
 
-class _HomeWebScreenState extends State<HomeWebScreen> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+class _HomeWebScreenState extends State<HomeWebScreen> with SingleTickerProviderStateMixin {
+  final List<String> tabs = [
+   'Items',
+    'Pricing',
+    'Info',
+    'Tasks',
+    'Analytics',
+  ];
+  late TabController _tabController;
+  @override
+  void initState() {
+    _tabController = TabController(length:5, vsync: this);
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('Flutter Demo Home Page'),
+      backgroundColor: AppColors.primaryBlackColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          CustomAppBar(tabController: _tabController, tabs: tabs),
+          Expanded(
+            child: TabBarView(
+              controller: _tabController,
+              children:  [
+                ItemsScreen(),
+                Center(child: Text('Pricing',style: AppTextStyles.fontWhite14W500,),),
+                Center(child: Text('Info',style: AppTextStyles.fontWhite14W500,),),
+                Center(child: Text('Tasks',style: AppTextStyles.fontWhite14W500,),),
+                Center(child: Text('Analytics',style: AppTextStyles.fontWhite14W500,),),
+              ],),
+          )
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:',),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+
